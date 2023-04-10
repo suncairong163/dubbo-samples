25d98ef898d0ed3c379a9abb5de16097cbf37532
suncr <suncairong@moresec.cn>
2023-04-10 13:41:29 +0800

    ExceptionMapperService

--- 3-extensions/protocol/dubbo-samples-rest/src/main/java/org/apache/dubbo/samples/rest/api/ExceptionMapperService.java
+++ 3-extensions/protocol/dubbo-samples-rest/src/main/java/org/apache/dubbo/samples/rest/api/ExceptionMapperService.java
@@ -20,13 +20,13 @@ package org.apache.dubbo.samples.rest.api;
 
 
 
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 @Path("/exception/mapper")
 public interface ExceptionMapperService {
 
-    @GET
+    @POST
     @Path("/exception")
     String exception(String message);
 
